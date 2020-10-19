3c6bc5fac597f6e4b3e26cbf22d3f60fd0bd61fe
ingvord <tla-ingvord@yandex.ru>
2020-10-19 08:13:53 +0300

    progress

--- src/message.js
+++ src/message.js
@@ -10,11 +10,14 @@
  * @see https://github.com/waltz-controls/rfc/blob/master/1/message.md
  */
 export class Message {
-    constructor({id, parent, user, source, target, payload}) {
+    constructor({id, parent, user, action, origin, target, payload}) {
+        if (!id) throw new Error("id must be specified")
+        if (!origin) throw new Error("origin must be specified")
         this.id = id;
         this.parent = parent;
         this.user = user;
-        this.origin = source;
+        this.action = action;
+        this.origin = origin;
         this.target = target;
         this.payload = payload;
     }
